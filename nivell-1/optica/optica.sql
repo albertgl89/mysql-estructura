9382d07134c8ad89966c30b4ae33aecc38f7d0c4
Albert Garcia Llorca <albertgarcia20@gmail.com>
2021-11-24 09:45:22 +0000

    Nivell 1 / Òptica - Exercici refet

--- nivell-1/optica/optica.sql
+++ nivell-1/optica/optica.sql
@@ -1,73 +1,194 @@
-CREATE SCHEMA `s2_e1_ex1` DEFAULT CHARACTER SET utf8 ;
+-- MySQL Workbench Forward Engineering
 
-CREATE TABLE `s2_e1_ex1`.`proveidors` (
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema optica
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `optica` ;
+
+-- -----------------------------------------------------
+-- Schema optica
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `optica` DEFAULT CHARACTER SET utf8 ;
+USE `optica` ;
+
+-- -----------------------------------------------------
+-- Table `optica`.`adreces`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `optica`.`adreces` ;
+
+CREATE TABLE IF NOT EXISTS `optica`.`adreces` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `carrer` VARCHAR(60) NOT NULL,
+  `numero` VARCHAR(10) NOT NULL,
+  `pis` VARCHAR(10) NULL,
+  `porta` VARCHAR(10) NULL,
+  `ciutat` VARCHAR(60) NOT NULL,
+  `codi_postal` VARCHAR(10) NOT NULL,
+  `pais` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `optica`.`proveidors`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `optica`.`proveidors` ;
+
+CREATE TABLE IF NOT EXISTS `optica`.`proveidors` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nom` VARCHAR(45) NOT NULL,
-  `carrer` VARCHAR(150) NOT NULL,
-  `numero` VARCHAR(5) NOT NULL,
-  `pis` VARCHAR(5) NULL,
-  `porta` VARCHAR(5) NULL,
-  `ciutat` VARCHAR(45) NOT NULL,
-  `codi_postal` VARCHAR(15) NOT NULL,
-  `pais` VARCHAR(45) NOT NULL,
-  `telefon` INT(20) NOT NULL,
-  `fax` INT(20) NULL,
-  `NIF` VARCHAR(45) NOT NULL,
+  `adreca` INT NOT NULL,
+  `telefon` INT NOT NULL,
+  `fax` INT NULL,
+  `nif` VARCHAR(20) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `NIF_UNIQUE` (`NIF` ASC));
+  UNIQUE INDEX `nif_UNIQUE` (`nif` ASC) ,
+  INDEX `FK_PROVADRECA_idx` (`adreca` ASC) ,
+  CONSTRAINT `FK_PROVADRECA`
+    FOREIGN KEY (`adreca`)
+    REFERENCES `optica`.`adreces` (`id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
 
-CREATE TABLE `s2_e1_ex1`.`ulleres` (
+
+-- -----------------------------------------------------
+-- Table `optica`.`clients`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `optica`.`clients` ;
+
+CREATE TABLE IF NOT EXISTS `optica`.`clients` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(100) NOT NULL,
+  `adreca` INT NOT NULL,
+  `telefon` VARCHAR(20) NOT NULL,
+  `email` VARCHAR(120) NOT NULL,
+  `registrat` DATE NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `FK_CLADRECA_idx` (`adreca` ASC) ,
+  CONSTRAINT `FK_CLADRECA`
+    FOREIGN KEY (`adreca`)
+    REFERENCES `optica`.`adreces` (`id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `optica`.`marques`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `optica`.`marques` ;
+
+CREATE TABLE IF NOT EXISTS `optica`.`marques` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `marca` VARCHAR(45) NOT NULL,
+  `proveidor` INT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `marca_UNIQUE` (`marca` ASC) ,
+  INDEX `FK_MARPROVEIDOR_idx` (`proveidor` ASC) ,
+  CONSTRAINT `FK_MARPROVEIDOR`
+    FOREIGN KEY (`proveidor`)
+    REFERENCES `optica`.`proveidors` (`id`)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `optica`.`ulleres`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `optica`.`ulleres` ;
+
+CREATE TABLE IF NOT EXISTS `optica`.`ulleres` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `marca` INT NOT NULL,
   `grad_esq` DECIMAL(4,2) NOT NULL,
   `grad_dreta` DECIMAL(4,2) NOT NULL,
-  `tipus` SET('flotant', 'pasta', 'metàl·lica') NOT NULL,
-  `color_muntura` VARCHAR(25) NOT NULL,
-  `color_v_esq` VARCHAR(25) NOT NULL,
-  `color_v_dret` VARCHAR(25) NOT NULL,
-  `preu` DECIMAL(6,4) NOT NULL,
-  PRIMARY KEY (`id`));
+  `muntura` SET('flotant', 'pasta', 'metàl·lica') NOT NULL,
+  `color` VARCHAR(45) NOT NULL,
+  `color_vidres` VARCHAR(45) NOT NULL,
+  `preu` DECIMAL(6,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `FK_ULMARQUES_idx` (`marca` ASC) ,
+  CONSTRAINT `FK_ULMARQUES`
+    FOREIGN KEY (`marca`)
+    REFERENCES `optica`.`marques` (`id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
 
 
-ALTER TABLE `s2_e1_ex1`.`ulleres` 
-ADD COLUMN `proveidor` INT NOT NULL AFTER `preu`,
-ADD INDEX `FK_PROVEIDOR_idx` (`proveidor` ASC);
-;
-ALTER TABLE `s2_e1_ex1`.`ulleres` 
-ADD CONSTRAINT `FK_PROVEIDOR`
-  FOREIGN KEY (`proveidor`)
-  REFERENCES `s2_e1_ex1`.`proveidors` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
+-- -----------------------------------------------------
+-- Table `optica`.`recomanacions`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `optica`.`recomanacions` ;
 
-CREATE TABLE `s2_e1_ex1`.`clients` (
+CREATE TABLE IF NOT EXISTS `optica`.`recomanacions` (
+  `client` INT NOT NULL,
+  `recomanat_per` INT NOT NULL,
+  PRIMARY KEY (`client`),
+  INDEX `FK_RECRECOMANAT_idx` (`recomanat_per` ASC) ,
+  CONSTRAINT `FK_RECCLIENT`
+    FOREIGN KEY (`client`)
+    REFERENCES `optica`.`clients` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `FK_RECRECOMANAT`
+    FOREIGN KEY (`recomanat_per`)
+    REFERENCES `optica`.`clients` (`id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `optica`.`empleats`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `optica`.`empleats` ;
+
+CREATE TABLE IF NOT EXISTS `optica`.`empleats` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `adreca` VARCHAR(250) NOT NULL,
-  `telefon` INT(20) NOT NULL,
-  `email` VARCHAR(80) NOT NULL,
-  `alta_registre` DATE NOT NULL,
-  `recomanat` INT NULL,
-  `ates` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`));
+  `nom` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
-ALTER TABLE `s2_e1_ex1`.`clients` 
-ADD INDEX `FK_RECOMANACIO_idx` (`recomanat` ASC);
-;
-ALTER TABLE `s2_e1_ex1`.`clients` 
-ADD CONSTRAINT `FK_RECOMANACIO`
-  FOREIGN KEY (`recomanat`)
-  REFERENCES `s2_e1_ex1`.`clients` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
 
-ALTER TABLE `s2_e1_ex1`.`ulleres` 
-ADD COLUMN `client` INT NULL AFTER `proveidor`,
-ADD INDEX `FK_CLIENT_idx` (`client` ASC) ;
-;
-ALTER TABLE `s2_e1_ex1`.`ulleres` 
-ADD CONSTRAINT `FK_CLIENT`
-  FOREIGN KEY (`client`)
-  REFERENCES `s2_e1_ex1`.`clients` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
+-- -----------------------------------------------------
+-- Table `optica`.`vendes`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `optica`.`vendes` ;
 
+CREATE TABLE IF NOT EXISTS `optica`.`vendes` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `ulleres` INT NOT NULL,
+  `client` INT NOT NULL,
+  `empleat` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `ulleres_UNIQUE` (`ulleres` ASC) ,
+  INDEX `FK_VCLIENT_idx` (`client` ASC) ,
+  INDEX `FK_VEMPLEATS_idx` (`empleat` ASC) ,
+  CONSTRAINT `FK_VULLERES`
+    FOREIGN KEY (`ulleres`)
+    REFERENCES `optica`.`ulleres` (`id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  CONSTRAINT `FK_VCLIENT`
+    FOREIGN KEY (`client`)
+    REFERENCES `optica`.`clients` (`id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  CONSTRAINT `FK_VEMPLEATS`
+    FOREIGN KEY (`empleat`)
+    REFERENCES `optica`.`empleats` (`id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
