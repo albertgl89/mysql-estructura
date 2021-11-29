8e124bb6e198d34a87cbef059cbecc5eb62370f8
Albert Garcia Llorca <albertgarcia20@gmail.com>
2021-11-29 09:07:57 +0000

    Nivell 1 / Exercici 1 Pizzeria refet

--- nivell-1/pizzeria/pizzeria.sql
+++ nivell-1/pizzeria/pizzeria.sql
@@ -1,235 +1,347 @@
-CREATE SCHEMA `s2_e1_ex1b` DEFAULT CHARACTER SET utf8 ;
+-- MySQL Workbench Forward Engineering
 
-CREATE TABLE `s2_e1_ex1b`.`clients` (
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema pizzeria
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `pizzeria` ;
+
+-- -----------------------------------------------------
+-- Schema pizzeria
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `pizzeria` DEFAULT CHARACTER SET utf8 ;
+USE `pizzeria` ;
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`localitats`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `pizzeria`.`localitats` ;
+
+CREATE TABLE IF NOT EXISTS `pizzeria`.`localitats` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nom` VARCHAR(45) NOT NULL,
-  `cognoms` VARCHAR(80) NOT NULL,
-  `adreca` VARCHAR(80) NOT NULL,
-  `codi_postal` VARCHAR(5) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`provincies`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `pizzeria`.`provincies` ;
+
+CREATE TABLE IF NOT EXISTS `pizzeria`.`provincies` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`localitats_provincia`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `pizzeria`.`localitats_provincia` ;
+
+CREATE TABLE IF NOT EXISTS `pizzeria`.`localitats_provincia` (
   `localitat` INT NOT NULL,
   `provincia` INT NOT NULL,
-  `telefon` INT(20) NOT NULL,
-  PRIMARY KEY (`id`));
+  PRIMARY KEY (`localitat`),
+  INDEX `FK_LPPROVINCIA_idx` (`provincia` ASC) ,
+  CONSTRAINT `FK_LPLOCALITAT`
+    FOREIGN KEY (`localitat`)
+    REFERENCES `pizzeria`.`localitats` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `FK_LPPROVINCIA`
+    FOREIGN KEY (`provincia`)
+    REFERENCES `pizzeria`.`provincies` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
 
-CREATE TABLE `s2_e1_ex1b`.`provincies` (
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`client`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `pizzeria`.`client` ;
+
+CREATE TABLE IF NOT EXISTS `pizzeria`.`client` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `provincia` VARCHAR(60) NOT NULL,
-  PRIMARY KEY (`id`));
+  `nom` VARCHAR(45) NOT NULL,
+  `cognoms` VARCHAR(90) NOT NULL,
+  `adreca` VARCHAR(90) NOT NULL,
+  `codi_postal` INT NOT NULL,
+  `localitat_provincia` INT NOT NULL,
+  `telefon` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `FK_CLLOCALITATPRO_idx` (`localitat_provincia` ASC) ,
+  CONSTRAINT `FK_CLLOCALITATPRO`
+    FOREIGN KEY (`localitat_provincia`)
+    REFERENCES `pizzeria`.`localitats_provincia` (`localitat`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
 
-CREATE TABLE `s2_e1_ex1b`.`localitats` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `localitat` VARCHAR(60) NOT NULL,
-  PRIMARY KEY (`id`));
 
-ALTER TABLE `s2_e1_ex1b`.`clients` 
-ADD INDEX `FK_LOCALITAT_idx` (`localitat` ASC) ,
-ADD INDEX `FK_PROVINCIA_idx` (`provincia` ASC) ;
-;
-ALTER TABLE `s2_e1_ex1b`.`clients` 
-ADD CONSTRAINT `FK_LOCALITAT`
-  FOREIGN KEY (`localitat`)
-  REFERENCES `s2_e1_ex1b`.`localitats` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION,
-ADD CONSTRAINT `FK_PROVINCIA`
-  FOREIGN KEY (`provincia`)
-  REFERENCES `s2_e1_ex1b`.`provincies` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
+-- -----------------------------------------------------
+-- Table `pizzeria`.`comandes`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `pizzeria`.`comandes` ;
 
-ALTER TABLE `s2_e1_ex1b`.`localitats` 
-ADD COLUMN `provincia` INT NOT NULL AFTER `localitat`,
-ADD INDEX `FK_PROV_idx` (`provincia` ASC) ;
-;
-ALTER TABLE `s2_e1_ex1b`.`localitats` 
-ADD CONSTRAINT `FK_PROV`
-  FOREIGN KEY (`provincia`)
-  REFERENCES `s2_e1_ex1b`.`provincies` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
+CREATE TABLE IF NOT EXISTS `pizzeria`.`comandes` (
+  `num_comanda` INT NOT NULL AUTO_INCREMENT,
+  `data_hora` TIMESTAMP NOT NULL,
+  `tipus` SET('domicili', 'recollir') NOT NULL,
+  `preu_total` DECIMAL(6,2) NOT NULL,
+  PRIMARY KEY (`num_comanda`))
+ENGINE = InnoDB;
 
-ALTER TABLE `s2_e1_ex1b`.`clients` 
-DROP FOREIGN KEY `FK_PROVINCIA`;
-ALTER TABLE `s2_e1_ex1b`.`clients` 
-ADD INDEX `FK_PROVINCIA_idx` (`provincia` ASC) ,
-DROP INDEX `FK_PROVINCIA_idx` ;
-;
-ALTER TABLE `s2_e1_ex1b`.`clients` 
-ADD CONSTRAINT `FK_PROVINCIA`
-  FOREIGN KEY (`provincia`)
-  REFERENCES `s2_e1_ex1b`.`localitats` (`provincia`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
 
-CREATE TABLE `s2_e1_ex1b`.`linies_comanda` (
+-- -----------------------------------------------------
+-- Table `pizzeria`.`realitza_comandes`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `pizzeria`.`realitza_comandes` ;
+
+CREATE TABLE IF NOT EXISTS `pizzeria`.`realitza_comandes` (
+  `comanda` INT NOT NULL,
+  `client` INT NOT NULL,
+  PRIMARY KEY (`comanda`),
+  INDEX `FK_RCCLIENT_idx` (`client` ASC) ,
+  CONSTRAINT `FK_RCCOMANDA`
+    FOREIGN KEY (`comanda`)
+    REFERENCES `pizzeria`.`comandes` (`num_comanda`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `FK_RCCLIENT`
+    FOREIGN KEY (`client`)
+    REFERENCES `pizzeria`.`client` (`id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`productes`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `pizzeria`.`productes` ;
+
+CREATE TABLE IF NOT EXISTS `pizzeria`.`productes` (
+  `producte` INT NOT NULL,
+  `descripcio` VARCHAR(250) NOT NULL,
+  `imatge` VARCHAR(120) NOT NULL,
+  `preu` DECIMAL(6,2) NOT NULL,
+  PRIMARY KEY (`producte`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`linies_comanda`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `pizzeria`.`linies_comanda` ;
+
+CREATE TABLE IF NOT EXISTS `pizzeria`.`linies_comanda` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `producte` INT NOT NULL,
   `quantitat` INT NOT NULL,
-  `total` DECIMAL(6,2) NOT NULL COMMENT 'SELECT `productes`.`preu` * `linies_comanda`.`quantitat` WHERE `linies_comanda`.`producte` = `productes`.`id`;',
-  PRIMARY KEY (`id`));
-
-CREATE TABLE `s2_e1_ex1b`.`comandes` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `client` INT NOT NULL,
-  `data_hora` TIMESTAMP NOT NULL COMMENT 'NOW()\n',
-  `tipus` SET('domicili', 'recollida') NOT NULL,
-  `total` DECIMAL(6,2) NOT NULL COMMENT 'SELECT SUM(`total`) FROM `s2_e1_ex1b`.`linies_comanda` WHERE `s2_e1_ex1b`.`linies_comanda`.`comanda` = `s2_e1_ex1b`.`comandes`.`id`;',
+  `total_linia` DECIMAL(6,2) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `FK_CLIENT_idx` (`client` ASC) ,
-  CONSTRAINT `FK_CLIENT`
-    FOREIGN KEY (`client`)
-    REFERENCES `s2_e1_ex1b`.`clients` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+  INDEX `FK_LCPRODUCTE_idx` (`producte` ASC) ,
+  CONSTRAINT `FK_LCPRODUCTE`
+    FOREIGN KEY (`producte`)
+    REFERENCES `pizzeria`.`productes` (`producte`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
 
-ALTER TABLE `s2_e1_ex1b`.`linies_comanda` 
-ADD COLUMN `comanda` INT NOT NULL AFTER `total`,
-ADD INDEX `FK_COMANDA_idx` (`comanda` ASC) ;
-;
-ALTER TABLE `s2_e1_ex1b`.`linies_comanda` 
-ADD CONSTRAINT `FK_COMANDA`
-  FOREIGN KEY (`comanda`)
-  REFERENCES `s2_e1_ex1b`.`comandes` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
 
-CREATE TABLE `s2_e1_ex1b`.`productes` (
+-- -----------------------------------------------------
+-- Table `pizzeria`.`categories_pizza`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `pizzeria`.`categories_pizza` ;
+
+CREATE TABLE IF NOT EXISTS `pizzeria`.`categories_pizza` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nom` VARCHAR(45) NOT NULL,
-  `descripcio` VARCHAR(100) NULL,
-  `imatge` VARCHAR(300) NULL COMMENT 'Suposem que emmagatzema la URL relativa als fitxers d\'imatge corresponent.',
-  `preu` DECIMAL(6,2) NOT NULL,
-  `categoria` INT NULL COMMENT 'Categoria si es pizza',
-  `tipus` SET('pizza', 'hamburguesa', 'beguda') NOT NULL,
-  PRIMARY KEY (`id`));
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
-ALTER TABLE `s2_e1_ex1b`.`linies_comanda` 
-CHANGE COLUMN `total` `total` DECIMAL(6,2) NOT NULL COMMENT 'SELECT (`productes`.`preu` * `linies_comanda`.`quantitat`) FROM `s2_e1_ex1b`.`productes` JOIN `s2_e1_ex1b`.`linies_comanda`  WHERE `linies_comanda`.`producte` = `productes`.`id`;' ,
-ADD INDEX `FK_PRODUCTE_idx` (`producte` ASC) ;
-;
-ALTER TABLE `s2_e1_ex1b`.`linies_comanda` 
-ADD CONSTRAINT `FK_PRODUCTE`
-  FOREIGN KEY (`producte`)
-  REFERENCES `s2_e1_ex1b`.`productes` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
 
-ALTER TABLE `s2_e1_ex1b`.`productes` 
-ADD INDEX `IDX_PREU` (`preu` ASC) ;
-;
+-- -----------------------------------------------------
+-- Table `pizzeria`.`pizzes`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `pizzeria`.`pizzes` ;
 
-ALTER TABLE `s2_e1_ex1b`.`linies_comanda` 
-ADD COLUMN `preu_unitari` DECIMAL(6,2) NOT NULL AFTER `comanda`,
-ADD CONSTRAINT `FK_PREU_U`
-  FOREIGN KEY (`preu_unitari`)
-  REFERENCES `s2_e1_ex1b`.`productes` (`preu`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-
-CREATE TABLE `s2_e1_ex1b`.`categories` (
+CREATE TABLE IF NOT EXISTS `pizzeria`.`pizzes` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nom` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`));
-
-ALTER TABLE `s2_e1_ex1b`.`productes` 
-ADD INDEX `FK_CAT_PIZZA_idx` (`categoria` ASC) ;
-;
-ALTER TABLE `s2_e1_ex1b`.`productes` 
-ADD CONSTRAINT `FK_CAT_PIZZA`
-  FOREIGN KEY (`categoria`)
-  REFERENCES `s2_e1_ex1b`.`categories` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-
-CREATE TABLE `s2_e1_ex1b`.`botigues` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `adreca` VARCHAR(80) NOT NULL,
-  `codi_postal` VARCHAR(5) NOT NULL,
-  `localitat` INT NOT NULL,
-  `provincia` INT NOT NULL,
+  `categoria` INT NULL,
   PRIMARY KEY (`id`),
-  INDEX `FK_LOC_idx` (`localitat` ASC) ,
-  INDEX `FK_PROV_idx` (`provincia` ASC) ,
-  CONSTRAINT `FK_BLOC`
-    FOREIGN KEY (`localitat`)
-    REFERENCES `s2_e1_ex1b`.`localitats` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `FK_BPROV`
-    FOREIGN KEY (`provincia`)
-    REFERENCES `s2_e1_ex1b`.`localitats` (`provincia`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+  INDEX `FK_PICATEGORIA_idx` (`categoria` ASC) ,
+  CONSTRAINT `FK_PIPRODUCTE`
+    FOREIGN KEY (`id`)
+    REFERENCES `pizzeria`.`productes` (`producte`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  CONSTRAINT `FK_PICATEGORIA`
+    FOREIGN KEY (`categoria`)
+    REFERENCES `pizzeria`.`categories_pizza` (`id`)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
 
-ALTER TABLE `s2_e1_ex1b`.`comandes` 
-ADD COLUMN `botiga` INT NOT NULL AFTER `total`,
-ADD INDEX `FK_BOTIGA_idx` (`botiga` ASC) ;
-;
-ALTER TABLE `s2_e1_ex1b`.`comandes` 
-ADD CONSTRAINT `FK_BOTIGA`
-  FOREIGN KEY (`botiga`)
-  REFERENCES `s2_e1_ex1b`.`botigues` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
 
-CREATE TABLE `s2_e1_ex1b`.`empleats` (
+-- -----------------------------------------------------
+-- Table `pizzeria`.`hamburgueses`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `pizzeria`.`hamburgueses` ;
+
+CREATE TABLE IF NOT EXISTS `pizzeria`.`hamburgueses` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nom` VARCHAR(45) NOT NULL,
-  `cognoms` VARCHAR(80) NOT NULL,
-  `telefon` INT(20) NOT NULL,
-  `NIF` VARCHAR(12) NOT NULL,
-  `rol` SET('cuiner', 'repartidor') NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `FK_HAPRODUCTE`
+    FOREIGN KEY (`id`)
+    REFERENCES `pizzeria`.`productes` (`producte`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`begudes`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `pizzeria`.`begudes` ;
+
+CREATE TABLE IF NOT EXISTS `pizzeria`.`begudes` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `FK_BEPRODUCTE`
+    FOREIGN KEY (`id`)
+    REFERENCES `pizzeria`.`productes` (`producte`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`botiga`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `pizzeria`.`botiga` ;
+
+CREATE TABLE IF NOT EXISTS `pizzeria`.`botiga` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `adreca` VARCHAR(90) NOT NULL,
+  `codi_postal` INT NOT NULL,
+  `localitat_provincia` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `FK_BOADRECA_idx` (`localitat_provincia` ASC) ,
+  CONSTRAINT `FK_BOADRECA`
+    FOREIGN KEY (`localitat_provincia`)
+    REFERENCES `pizzeria`.`localitats_provincia` (`localitat`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`gestiona_comandes`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `pizzeria`.`gestiona_comandes` ;
+
+CREATE TABLE IF NOT EXISTS `pizzeria`.`gestiona_comandes` (
+  `comanda` INT NOT NULL,
   `botiga` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `FK_EBOTIGA_idx` (`botiga` ASC) ,
-  CONSTRAINT `FK_EBOTIGA`
+  PRIMARY KEY (`comanda`),
+  INDEX `FK_GCBOTIGA_idx` (`botiga` ASC) ,
+  CONSTRAINT `FK_GCCOMANDA`
+    FOREIGN KEY (`comanda`)
+    REFERENCES `pizzeria`.`comandes` (`num_comanda`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `FK_GCBOTIGA`
     FOREIGN KEY (`botiga`)
-    REFERENCES `s2_e1_ex1b`.`botigues` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    REFERENCES `pizzeria`.`botiga` (`id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
 
-ALTER TABLE `s2_e1_ex1b`.`comandes` 
-ADD COLUMN `repartidor` INT NULL AFTER `botiga`,
-ADD INDEX `FK_REPARTIDOR_idx` (`repartidor` ASC) ;
-;
-ALTER TABLE `s2_e1_ex1b`.`comandes` 
-ADD CONSTRAINT `FK_REPARTIDOR`
-  FOREIGN KEY (`repartidor`)
-  REFERENCES `s2_e1_ex1b`.`empleats` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
 
-CREATE TABLE `s2_e1_ex1b`.`lliuraments_domicili` (
+-- -----------------------------------------------------
+-- Table `pizzeria`.`empleats`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `pizzeria`.`empleats` ;
+
+CREATE TABLE IF NOT EXISTS `pizzeria`.`empleats` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `repartidor` INT NOT NULL,
-  `data_hora` TIMESTAMP NOT NULL,
+  `nom` VARCHAR(45) NOT NULL,
+  `cognoms` VARCHAR(90) NOT NULL,
+  `nif` VARCHAR(15) NOT NULL,
+  `telefon` INT NOT NULL,
+  `rol` SET('cuiner', 'repartidor') NOT NULL,
+  `botiga` INT NULL,
   PRIMARY KEY (`id`),
-  INDEX `FK_LLREPARTIDOR_idx` (`repartidor` ASC) ,
-  CONSTRAINT `FK_LLREPARTIDOR`
-    FOREIGN KEY (`repartidor`)
-    REFERENCES `s2_e1_ex1b`.`empleats` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
-ALTER TABLE `s2_e1_ex1b`.`comandes` 
-DROP FOREIGN KEY `FK_REPARTIDOR`;
-ALTER TABLE `s2_e1_ex1b`.`comandes` 
-CHANGE COLUMN `repartidor` `dades_lliurament_domicili` INT(11) NULL DEFAULT NULL ,
-ADD INDEX `FK_LLDOMICILI_idx` (`dades_lliurament_domicili` ASC) ,
-DROP INDEX `FK_REPARTIDOR_idx` ;
-;
-ALTER TABLE `s2_e1_ex1b`.`comandes` 
-ADD CONSTRAINT `FK_LLDOMICILI`
-  FOREIGN KEY (`dades_lliurament_domicili`)
-  REFERENCES `s2_e1_ex1b`.`lliuraments_domicili` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
+  UNIQUE INDEX `nif_UNIQUE` (`nif` ASC) ,
+  INDEX `FK_EMBOTIGA_idx` (`botiga` ASC) ,
+  CONSTRAINT `FK_EMBOTIGA`
+    FOREIGN KEY (`botiga`)
+    REFERENCES `pizzeria`.`botiga` (`id`)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `pizzeria`.`reparteix`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `pizzeria`.`reparteix` ;
+
+CREATE TABLE IF NOT EXISTS `pizzeria`.`reparteix` (
+  `comanda` INT NOT NULL,
+  `empleat` INT NOT NULL,
+  `data_hora` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`comanda`),
+  INDEX `FK_REEMPLEAT_idx` (`empleat` ASC) ,
+  CONSTRAINT `FK_RECOMANDA`
+    FOREIGN KEY (`comanda`)
+    REFERENCES `pizzeria`.`gestiona_comandes` (`comanda`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `FK_REEMPLEAT`
+    FOREIGN KEY (`empleat`)
+    REFERENCES `pizzeria`.`empleats` (`id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `pizzeria`.`tickets`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `pizzeria`.`tickets` ;
+
+CREATE TABLE IF NOT EXISTS `pizzeria`.`tickets` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `comanda` INT NOT NULL,
+  `linia` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `linia_UNIQUE` (`linia` ASC) ,
+  UNIQUE INDEX `comanda_UNIQUE` (`comanda` ASC) ,
+  CONSTRAINT `FK_TICOMANDA`
+    FOREIGN KEY (`comanda`)
+    REFERENCES `pizzeria`.`comandes` (`num_comanda`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `FK_TILINIA`
+    FOREIGN KEY (`linia`)
+    REFERENCES `pizzeria`.`linies_comanda` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
 
 
-
-
-
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
